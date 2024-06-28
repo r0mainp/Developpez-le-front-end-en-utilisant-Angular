c2d0cb6c9feb80b05c7a5e0792f5a2fb2d8fa6cc
RomainP <romainportier7@gmail.com>
2024-06-28 16:38:33 +0200

    add new detail page + routing

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
 
@@ -13,9 +14,9 @@ export class PieChartComponent implements OnInit{
 
   view: [number, number] = [700, 300];
 
-  formattedData!: {name: string, value: number}[];
+  formattedData!: {name: string, value: number, extra: {id: number}}[];
 
-  constructor(){
+  constructor(private router: Router){
     
   }
 
@@ -23,12 +24,20 @@ export class PieChartComponent implements OnInit{
     this.formatData()
     Object.assign(this, this.formattedData)
   }
+
+  redirectToDetails(country: {name: string, value: number, extra: {id: number}}){
+    this.router.navigateByUrl(`details/${country.extra.id}`)
+  }
+
   private formatData(){
     this.formattedData = this.olympics.map( data => {
       const totalMedal = data.participations.reduce((total, participation) => total + participation.medalsCount, 0)
       return {
         name: data.country,
-        value: totalMedal
+        value: totalMedal,
+        extra: {
+          id: data.id,
+        }
       }
     })
   }
